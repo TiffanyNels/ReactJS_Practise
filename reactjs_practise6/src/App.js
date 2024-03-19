12fcde51883e30d9e32db34471c80949a8eb2dc5
Tiffany Nels <tnelsweb@gmail.com>
2024-03-19 16:35:10 +0200

    Final commit

---
+++ reactjs_practise6/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+import { Contact } from './pages/Contact';
+import { Navbar } from "./Navbar";
+import { useState, createContext } from 'react';
+
+export const AppContext = createContext();
+
+function App() {
+  const [username, setUserName] = useState("");
+
+  return (
+    <div className="App">
+      <AppContext.Provider value={{username, setUserName}}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>404 ERROR</h1>} />
+        </Routes>
+      </Router>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
