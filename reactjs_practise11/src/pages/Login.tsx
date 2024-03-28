4ca09b41307e69ae24b00ac3fc638bcfdc9eb102
Tiffany Nels <tnelsweb@gmail.com>
2024-03-28 19:48:59 +0200

    Final commit

---
+++ reactjs_practise11/src/pages/Login.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { login, logout } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+
+export const Login = () => {
+    const [newUserName, setNewUsername] = useState<string>("");
+
+    const dispatch = useDispatch();
+    const username = useSelector((state: any) => state.user.value.username);
+
+    return <h1>{username}
+        <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setNewUsername(e.target.value);
+        }}/>
+        <button onClick={() => dispatch(login({ username: newUserName }))}> Login </button>
+        <button onClick={() => dispatch(logout())}> Logout </button>
+    </h1>
+};
